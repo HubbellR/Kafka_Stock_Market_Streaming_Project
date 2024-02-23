15929fb6440ce9a9618ac811616040081a2051b0
HubbellR <hubbell.rowe@gmail.com>
2024-02-23 14:57:28 -0500

    Test commit 01 :). Committing all files to respository.

---
+++ Fake_Stock_market_Data_Producer.py
@@ -0,0 +1,26 @@
+#Using this code to create data in producer side of Kafka
+import pandas as pd
+from kafka import KafkaProducer
+from time import sleep
+from json import dumps
+import json
+
+producer = KafkaProducer(bootstrap_servers=['16.16.141.101:9092'],
+        value_serializer = lambda x:
+        dumps(x).encode('utf-8'),
+        api_version=(0, 10, 1))
+
+#producer.send('demo_test', value={'hello':'world'})
+
+df_in = pd.read_csv("/home/hubbell/PlayingFiles/Data_Eng/Kafka_Stock_Market_Real_Time_Project_Files/indexProcessed.csv")
+
+df_in.head()
+
+while True:
+    dict_stock = df_in.sample(1).to_dict(orient="records")[0]
+
+    producer.send('demo_test', value= dict_stock)
+    
+    sleep(1)
+
+producer.flush()
---
+++ Stock_Consumer_code.py
@@ -0,0 +1,25 @@
+from kafka import KafkaConsumer
+from time import sleep
+from json import dumps, loads
+import json
+from s3fs import S3FileSystem
+
+consumer = KafkaConsumer(
+        'demo_test',
+        bootstrap_servers = ['16.16.141.101:9092'],
+        value_deserializer= lambda x: loads(x.decode('utf-8')),
+        api_version=(0, 10, 1))    
+
+#for c in consumer:
+#    print(c.value)
+
+s3 = S3FileSystem()
+
+for count, i in enumerate(consumer):
+    print(i)
+    with s3.open("s3://hwr-stock-market-kafka-proj/stock_market_{}.json".format(count), 'w') as file:
+        json.dump(i.value, file)
+
+
+
+
